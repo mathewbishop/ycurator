c8fd3ee6c1061a2a00a91b75cddff3215d566b51
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2020-02-23 16:57:34 -0600

    Footer styles.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,7 @@
 <footer class="site-footer">
-    <p>Y-Curator</p>
-    <small class="is-block">A curator for Hacker News articles.</small>
-    <small class="is-block">Built by <a href="https://github.com/mathewbishop">Mathew Bishop</a></small>
+    <p style="color:#263d42;font-weight:700;">Y-Curator</p>
+    <small style="color:#263d42;font-weight:700;" class="is-block">A curator for Hacker News articles.</small>
+    <small style="color:#263d42;font-weight:700;" class="is-block">Built by <a class="by-line-link"
+            href="https://github.com/mathewbishop">Mathew
+            Bishop</a></small>
 </footer>
