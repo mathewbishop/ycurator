d71db3430edd846c88f8f1b73b97c9cd7742737d
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2020-03-23 22:06:55 -0500

    WIP

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,3 @@
-$(document).ready(function () {
-    var path = window.location.pathname
-    $(".main-nav").find(".nav-active").removeClass("nav-active")
-    $(".main-nav .nav-link").each(function () {
-        if ($(this).attr("href") === path) {
-            $(this).addClass("nav-active")
-        }
-    })
-
-    $(".btn-save-article").on("click", function (e) {
-        e.preventDefault();
-        var articleIndex = $(this).attr("id").split("_")[1]
-        var articleObj = {
-            title: $("#title_" + articleIndex).text().trim(),
-            commentCount: $("#comment-count_" + articleIndex).attr("data-comment-count"),
-            articleUrl: $("#article-link_" + articleIndex).attr("href"),
-            discussionUrl: $("#disc-link_" + articleIndex).attr("href")
-        }
-
-        $.ajax({
-            type: "POST",
-            url: "http://ycurator.test/save-article",
-            data: articleObj,
-            dataType: "application/json",
-            success: function (res) {
-                console.log(res)
-            },
-            error: function (err) {
-                alert("An error has occured. Check console.")
-                console.log(err)
-            }
-        })
-
-    })
-});
-
 function GetCurrentArticles() {
     $(".lds-dual-ring, .loading-overlay").show()
 
@@ -76,5 +40,40 @@ function GetCurrentArticles() {
 
 
 $(document).ready(function () {
-    GetCurrentArticles()
+    var path = window.location.pathname
+    $(".main-nav").find(".nav-active").removeClass("nav-active")
+    $(".main-nav .nav-link").each(function () {
+        if ($(this).attr("href") === path) {
+            $(this).addClass("nav-active")
+        }
+    })
+
+    $(".btn-save-article").on("click", function (e) {
+        e.preventDefault();
+        var articleIndex = $(this).attr("id").split("_")[1]
+        var articleObj = {
+            title: $("#title_" + articleIndex).text().trim(),
+            commentCount: $("#comment-count_" + articleIndex).attr("data-comment-count"),
+            articleUrl: $("#article-link_" + articleIndex).attr("href"),
+            discussionUrl: $("#disc-link_" + articleIndex).attr("href")
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "http://ycurator.test/save-article",
+            data: articleObj,
+            dataType: "application/json",
+            success: function (res) {
+                console.log(res)
+            },
+            error: function (err) {
+                alert("An error has occured. Check console.")
+                console.log(err)
+            }
+        })
+    })
+
+    // Get articles on page load
+    GetCurrentArticles();
 });
+
