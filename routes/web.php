584b98b7a0dee3ca8ceb0c8bffd7e87df1e5316a
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2020-04-09 22:07:33 -0500

    WIP style login and register pages.

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Route::get('/criteria', function () {
     return view('criteria');
 })->middleware("auth");
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
