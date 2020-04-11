3e897d9f71726da2172e29f5e6ac0c2b1dd2e40b
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2020-04-11 15:40:06 -0500

    Change nave item 'home' name, and add descriptions for criteria controls.

--- resources/views/criteria.blade.php
+++ resources/views/criteria.blade.php
@@ -23,6 +23,7 @@
                         placeholder="Type keyword..">
                     <button id="btn-add-keyword" class="button is-small is-success is-rounded">Add Keyword</button>
                 </div>
+                <small class="is-block is-italic" style="margin-bottom:20px;">Articles with these keywords in the title will be curated.</small>
                 <button id="btn-del-keywords" class="button is-small is-rounded is-danger has-text-weight-bold"
                     style="visibility:hidden;margin-bottom:10px;">Delete Selected
                     (<span id="selected-count"></span>)</button>
@@ -30,10 +31,13 @@
 
                 </ul>
             </section>
-            <section class="threshold-section">
-                <h2 class="criteria__section-title">Comment Threshold</h2>
-                <input type="text" id="threshold-input" name="threshold-input" class="input is-small is-rounded">
-                <button id="btn-set-threshold" class="button is-small is-rounded is-success">Set Threshold</button>
+            <section>
+                <div class="threshold-controls">
+                    <h2 class="criteria__section-title">Comment Threshold</h2>
+                    <input type="text" id="threshold-input" name="threshold-input" class="input is-small is-rounded">
+                    <button id="btn-set-threshold" class="button is-small is-rounded is-success">Set Threshold</button>
+                </div>
+                <small class="is-block is-italic">Articles with a comment count greater than the threshold number will be curated, irrespective of keyword match.</small>
             </section>
         </main>
 
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,4 @@
 <nav class="main-nav">
-    <a class="button nav-link nav-active" href="/">Home</a>
-    {{-- <a class="button nav-link" href="/saved-articles">Saved Articles</a> --}}
+    <a class="button nav-link nav-active" href="/">Articles</a>
     <a class="button nav-link" href="/criteria">Curation Criteria</a>
 </nav>
