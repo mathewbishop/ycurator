41d15a7460fc18e8af57854cfafcf445c2c39671
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2020-04-17 20:59:45 -0500

    Change keywords table name to user_keywords, re-create user_comment_threshold table via migration.

--- app/UserCriteria.php
+++ app/UserCriteria.php
@@ -11,19 +11,19 @@ class UserCriteria extends Model
     // Keyword Methods
     public function SelectKeywordsByUser($user_id)
     {
-        $user_keywords = DB::table('keywords')->where('user_id', '=', $user_id)->get();
+        $user_keywords = DB::table('user_keywords')->where('user_id', '=', $user_id)->get();
         return $user_keywords;
     }
 
     public function InsertOneKeyword($user_id, $keyword)
     {
-        DB::table('keywords')->insert(['user_id' => $user_id, 'keyword' => $keyword]);
+        DB::table('user_keywords')->insert(['user_id' => $user_id, 'keyword' => $keyword]);
     }
 
     public function DeleteKeywordsById($keyword_id_list)
     {
         foreach ($keyword_id_list as $keyword_id) {
-            DB::table('keywords')->where('id', '=', $keyword_id)->delete();
+            DB::table('user_keywords')->where('id', '=', $keyword_id)->delete();
         }
     }
 
