8a45b6fddc8017c81fe70e28647fe15288ac6fa6
Mathew Bishop <39043451+mathewbishop@users.noreply.github.com>
2020-05-02 20:36:01 -0500

    Install bulma via npm, switch to using webpack to compile client side code.

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,10 +2,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ secure_asset('/css/bulma.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('/css/stylesheet.css') }}">
-    <script src="{{ secure_asset('/js/jquery-3.4.1.min.js') }}"></script>
-    <script src="{{ secure_asset('/js/main.js') }}"></script>
+    <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}">
+    <script src="{{ secure_asset('/js/app.js') }}"></script>
     <title>Y-Curator</title>
     @if(Auth::check())
     <script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ secure_asset('/css/bulma.css') }}">
-    <link href="{{ secure_asset('css/stylesheet.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -24,35 +23,6 @@
                 <a href="{{ url('/') }}">
                     Y-Curator
                 </a>
-
-                <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    
-                    <ul class="navbar-nav ml-auto">
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div> -->
             </div>
         </nav>
 
